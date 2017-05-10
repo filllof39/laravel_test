204cd0f1b06e48d4f420c7ec9ba50d2d29f16514
filllof39 <fillof39@gmail.com>
2017-05-10 23:23:35 +0300

    добавление debugbar laravel-tracy

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+		Recca0120\LaravelTracy\LaravelTracyServiceProvider::class,
 
         /*
          * Package Service Providers...
